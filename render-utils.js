0539e2dde130a68062b9b24d5570f063b357ceed
Khayman King <khaymanak@gmail.com>
2022-03-17 13:00:28 -0700

    saving before lunch

--- render-utils.js
+++ render-utils.js
@@ -2,14 +2,13 @@ export function renderMonster(monsterData) {
     const monstersEl = document.createElement('div');
     const monsternameEl = document.createElement('p');
     const monsterhpEl = document.createElement('p');
-    const monsterImg = document.createElement('img');
+    
 
     monstersEl.classList.add('monster');
 
     monsternameEl.textContent = monsterData.name;
     monsterhpEl.id = `monster-hp-${monsterData.id}`;
     monsterhpEl.textContent = monsterData.hp < 0 ? 0 : monsterData.hp;
-    monsterImg.id = ;
     if (monsterData.hp < 0){
         monstersEl.classList.add('defeated');
     }
