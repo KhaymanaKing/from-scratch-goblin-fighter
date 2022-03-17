13cd76d678c733698c0a3fef56b765ed8dffef28
Khayman King <khaymanak@gmail.com>
2022-03-17 11:21:02 -0700

    made monster render

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 // import functions and grab DOM elements
+import { renderMonster } from "./render-utils";
+
+
 const form = document.querySelector('#form');
 const monstersEl = document.querySelector('#form');
 const hitpointsEl = document.querySelector('#player-hp');
@@ -11,9 +14,22 @@ let monsters = [
     { id: 1, name: 'Glurp', hp: 15 },
     { id: 2, name: 'Terry', hp: 30 },
 ];
-
 let currentId = 3;
 // set event listeners 
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const monsterName = data.get('monster-name');
+
+    const newMonster = {
+        id: currentId,
+        name: monsterName,
+        hp: Math.ceil(Math.random() * 100),
+    };
+    currentId++;
+    monsters.push(newMonster);
+});
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
---
+++ render-utils.js
@@ -0,0 +1,18 @@
+export function renderMonster(monsterData) {
+    const monstersEl = document.createElement('div');
+    const monsternameEl = document.createElement('p');
+    const monsterhpEl = document.createElement('p');
+
+    monstersEl.classList.add('monster');
+
+    monsternameEl.textContent = monsterData.name;
+    monsterhpEl.id = `monster-hp-${monsterData.id}`;
+    monsterhpEl.textContent = monsterData.hp < 0 ? 0 : monsterData.hp;
+
+    if (monsterData.hp < 0){
+        monstersEl.classList.add('defeated');
+    }
+
+    monstersEl.append (monsternameEl, monsterhpEl);
+    return monstersEl;
+}
