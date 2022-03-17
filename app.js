e81934e59b9338ab78a12f081680a4d81fd58be2
Khayman King <khaymanak@gmail.com>
2022-03-17 10:56:26 -0700

    grabbed DOM and set state

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 // import functions and grab DOM elements
+const form = document.querySelector('#form');
+const monstersEl = document.querySelector('#form');
+const hitpointsEl = document.querySelector('#player-hp');
+const experienceEl = document.querySelector('#experience');
 
 // let state
+let hp = 0;
+let xp = 0;
+let monsters = [
+    { id: 1, name: 'Glurp', hp: 15 },
+    { id: 2, name: 'Terry', hp: 30 },
+];
 
+let currentId = 3;
 // set event listeners 
   // get user input
   // use user input to update state 
