8547850a9194a3aaad92e41686f1ae3ddb3620eb
Khayman King <khaymanak@gmail.com>
2022-03-18 10:42:52 -0700

    added slime monster animations

--- render-utils.js
+++ render-utils.js
@@ -3,11 +3,13 @@ export function renderMonster(monsterData) {
     const monsternameEl = document.createElement('p');
     const monsterhpEl = document.createElement('p');
     
+    
 
     monsterEl.classList.add('monster');
 
     monsternameEl.textContent = monsterData.name;
     monsterhpEl.textContent = monsterData.hp;
+    
 
     if (monsterData.hp < 0) {
         monsterEl.classList.add('defeated');
